d391d14b15d6fce452810c5d4b5d1a3cbb8a5556
TheycallmeNate <nathanmangal@gmail.com>
2021-05-06 18:45:48 -0400

    completed GET /posts/:id to fetch a single post

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const express = require("express");
 
 const app = express();
 
+// Middleware
 app.use(express.json());
 
+// Post request to create a new post
 app.post("/posts", (req, res) => {
   posts.push(req.body);
   let stringedData = JSON.stringify(posts, null, 2);
@@ -19,6 +21,19 @@ app.post("/posts", (req, res) => {
   return res.status(201).json({ message: "new post created" });
 });
 
+// Get request to fetch all posts
 app.get("/posts", (req, res) => res.status(200).json({ posts }));
+
+// Get request to fetch a single post by id parameter
+app.get("/posts/:id", (req, res) => {
+  let id = req.params.id;
+  let foundPost = posts.find((post) => String(post.id) === id);
+
+  if (foundPost) {
+    return res.status(200).json({ post: foundPost });
+  } else {
+    return res.status(404).json({ message: "post not found" });
+  }
+});
 
 app.listen(3000, () => console.log("Server is up and running."));
